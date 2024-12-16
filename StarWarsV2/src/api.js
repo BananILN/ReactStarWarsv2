5b89224b55b1b7541668fca0307ec126de4abf3f
BananILN <daniil.klovans84@mail.ru>
2024-12-16 22:22:25 +0400

    add new api RickAndMorty

--- StarWarsV2/src/api.js
+++ StarWarsV2/src/api.js
@@ -1,8 +1,15 @@
 export const fetchEpisode = () => {
-   return fetch('https://swapi-api.hbtn.io/api/films')
+   return fetch('https://rickandmortyapi.com/api/episode')
     .then((resposne) => resposne.json())
     .then((data)=>{
         return data.results;
     })
 }
+
+export const fetchHero = (ids) =>{
+    return  fetch(`https://rickandmortyapi.com/api/character/${ids.join(",")}`)
+    .then((response) => response.json())
+    
+    };
+
 
--- StarWarsV2/src/components/AppStarWars.jsx
+++ StarWarsV2/src/components/AppStarWars.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { fetchEpisode } from "../api"
+import { fetchEpisode, fetchHero } from "../api"
 import './Star.css'
 
 
@@ -16,36 +16,42 @@ export default function AppStarWars(){
 })
 
     const handleEpisodeClick = (episode) =>{
-        const hero = []
-        episode.characters.forEach(element => {
-            fetch(element)
-            .then((response) => response.json())
-            .then((data) => hero.push(data));
-        });
-        setCharacterEpisode({...characterEpisode, [episode.episode_id] : hero})
+            const ids = episode.characters.map((character) =>{
+                const id = character.split("/").pop();
+                return id;
+            });
+
+            fetchHero(ids).then((data)=>{
+                console.log(data);
+                setCharacterEpisode({...characterEpisode, [episode.id] :data })
+            })
+        };
+
+
+
     
-    }    
+      
     return (
         <>
         {episode.map((episode)=>{
             return <div
              className="episode"
-             key={episode.episode_id}
+             key={episode.id}
              onClick={() => handleEpisodeClick(episode)}>
 
                 <h2>Title:{episode.title}</h2>
                 <div className="character-container">
-                    {characterEpisode[episode.episode_id]?.map((character) =>{
+                    {characterEpisode[episode.id]?.map((character) =>{
                         return (
-                            <div key={character.url + episode.episode_id} className="episode">
+                            <div key={character.id} className="episode">
                                 <div className="character-left">
-                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1388px-Star_Wars_Logo.svg.png" alt="" />
+                                    <img src={character.image} alt="" />
                                 </div>
                                 <div className="character-right">
                                     <h3>{character.name}</h3>
-                                    <div>Цвет волос: {character.hair_color} </div>
-                                    <div>Цвет глаз : {character.eye_color} </div>
-                                    <div>Рост: {character.height} </div>
+                                    <div>Пол: {character.gender} </div>
+                                    <div>Bio: {character.species} </div>
+                                    <div>Location: {character.location.name} </div>
                                 </div>
                             </div>
                         )
