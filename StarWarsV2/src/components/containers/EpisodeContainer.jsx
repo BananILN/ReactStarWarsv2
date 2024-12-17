4794167e50ac0c6f0d8fb22d595d74a8055872c6
BananILN <daniil.klovans84@mail.ru>
2024-12-17 22:52:16 +0400

    get EpisodeCont

---
+++ StarWarsV2/src/components/containers/EpisodeContainer.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+import { fetchEpisode } from "../../api"
+import { EpisodeList } from "../presentation/EpisodeList"
+
+export const EpisodeContainer = () =>{
+    const [episodes, setEpisodes] = useState([])
+    useEffect (()=>{
+        fetchEpisode().then((data)=>{
+            setEpisodes(data)
+        })
+    },[])
+
+    return (
+        <EpisodeList episodes={episodes} />
+    ) 
+}
