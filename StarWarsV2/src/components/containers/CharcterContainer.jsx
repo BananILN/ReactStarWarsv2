0fc6ffffbf0aaed961580dcf206fbdd74d9f233d
BananILN <daniil.klovans84@mail.ru>
2024-12-17 22:42:40 +0400

    get characterCont

--- StarWarsV2/src/components/containers/CharcterContainer.jsx
+++ StarWarsV2/src/components/containers/CharcterContainer.jsx
@@ -1,3 +1,19 @@
+import { useEffect } from "react";
+import { fetchHero } from "../../api";
+import { CharacterList } from "../presentation/CharacterList";
+
 export const CharacterContainer =({ids}) =>{
-    
+    const [character, setChatacters] = useState([]);
+    const [isLoading,setLoading] = useState(true);
+
+    useEffect(()=>{
+        setLoading(true)
+        fetchHero(ids).then((data)=>{
+            console.log(data);
+            setChatacters(data);
+            setLoading(false);   
+        })
+    },[ids])
+
+    return <CharacterList character={character} isLoading={isLoading}/>
 }
